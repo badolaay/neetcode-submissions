49692e3b0c74da2163b231b7ca87ea2e0e4d39f6
badolaay <21973302+badolaay@users.noreply.github.com>
2026-04-28 21:26:52 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String encode(List<String> strs) {
+        if (strs == null || strs.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder output = new StringBuilder("");
+        String delimiter = ":";
+        for (String s : strs) {
+            output.append(s.length()).append(delimiter).append(s);
+        }
+
+        return output.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList();
+        String delimiter = ":";
+        // 5:apple6:orange5:hello5:world
+        for (int i = 0; i < str.length();) {
+            int delimiterIndex = str.indexOf(delimiter, i);
+            int beginIndex = delimiterIndex + 1;
+            int endIndex = beginIndex + Integer.parseInt(str.substring(i, delimiterIndex));
+
+            result.add(str.substring(beginIndex, endIndex));
+
+            i = endIndex;
+        }
+
+        return result;
+    }
+}
